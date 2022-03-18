99a419fe661acfb88b7b4366f8b08b49e03fe691
Phyrillis <101859275+Phyrillis@users.noreply.github.com>
2022-03-18 17:43:39 -0400

    Update music.py

--- music.py
+++ music.py
@@ -22,7 +22,7 @@ class music(commands.Cog):
   def search_yt(self, item):
       with YoutubeDL(self.YDL_OPTIONS) as ydl:
           try: 
-              info = ydl.extract_info("ytsearch:%s" % item, url, download=False)['entries'][0]
+              info = ydl.extract_info("ytsearch:%s" % item, download=False)['entries'][0]
           except Exception: 
               return False
 
